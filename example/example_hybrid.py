3b4753307b2217c2087acd5699587371d394fe4e
もやし <81414605+Madoa5561@users.noreply.github.com>
2026-01-26 14:08:32 +0900

    Update example_hybrid.py

--- example/example_hybrid.py
+++ example/example_hybrid.py
@@ -5,12 +5,12 @@ from argparse import ArgumentParser
 from flask import Flask, request, abort, send_from_directory
 from werkzeug.middleware.proxy_fix import ProxyFix
 import requests
-from LINELib.util import (
+from lineoa.util import (
     link_group_and_chat,
     get_chatid_from_groupid,
     get_groupid_from_chatid
 )
-from LINELib.linebot import LineBot
+from lineoa.linebot import LineBot
 
 from linebot.v3 import (
     WebhookHandler
